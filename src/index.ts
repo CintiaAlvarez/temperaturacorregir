f706a3f93df73eab03b1f081f4c3dd63e44efa19
CintiaAlvarez <104107300+CintiaAlvarez@users.noreply.github.com>
2022-05-08 02:33:37 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let dato : number;
+
+let max: number;
+max = dato;
+
+  if ( max = dato){
+    dato = prompt ("ingresa una temperatura")
+   console.log ("La temperatura maxima es: " + max );
+  
+  while ( dato != 0){
+   max=dato;
+}
